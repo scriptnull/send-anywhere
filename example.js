31db6e74cb5d2c750358caaeab57c6d07372cc83
Vishnu Bharathi <vishnubharathi04@gmail.com>
2015-08-19 15:22:59 +0530

    removed examples and improved docs

--- example.js
+++
@@ -1,9 +0,0 @@
-var sa = require('./index.js');
-var API_KEY = "db61271267a777500b86290005fc16b346b34049";
-var PROFILE_NAME = 'scriptnul';
-sa.sendFile(API_KEY , PROFILE_NAME , 'package.json' , function(err , details){
-	console.log(details);
-} , function(err , completedObj){
-	console.log('completed');
-	console.log(completedObj);
-});
--- receive.js
+++
@@ -1,7 +0,0 @@
-var sa = require('./index.js');
-var API_KEY = "db61271267a777500b86290005fc16b346b34049";
-var PROFILE_NAME = 'scriptnul2';
-sa.receiveAsData(API_KEY , PROFILE_NAME , '475077' ,function(err , data){
-	if(err)console.log(err.error);
-	else console.log(data);
-});
