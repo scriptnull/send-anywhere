986b1638e2ebf3caf1dea23180cde62ef678da0b
Vishnu Bharathi <vishnubharathi04@gmail.com>
2015-08-19 14:00:48 +0530

    send function working

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var fs = require('fs');
 var request = require('request');
 
-module.exports.sendFile = function(API_KEY , PROFILE_NAME ,FILE_PATH , cb){
+module.exports.sendFile = function(API_KEY , PROFILE_NAME ,FILE_PATH , sendConfigCb , sendCompletedcb){
 	var KEY_REQ_URL = 'https://send-anywhere.com/web/v1/device?api_key='+ API_KEY +'&profile_name=' + PROFILE_NAME;
 	var SEND_URL = 'https://send-anywhere.com/web/v1/key';
 	bridge.cookieInit(KEY_REQ_URL , function(cookie){
 		var keyGenOptions = bridge.getRequestOptionsWithCookie(SEND_URL , cookie );
 		request.get(keyGenOptions , function(err , response , body){
-			if(err) return cb({ error : err , response : response } , null );
-			if(response.statuseCode == 200){
+			if(err) sendConfigCb({ error : err , response : response } , null );
+			else sendConfigCb(null , JSON.parse(body));
+			if(response.statusCode == 200){
 				var formData = {
 					file: fs.createReadStream(FILE_PATH)
 				}			
 				body = JSON.parse(body);
 				var sendUrl = body.weblink;
 				request.post({ url : sendUrl , formData :  formData} , function(err , response , body ){
-					if(err) return cb({ error : err , response : response } , null );
-					else return cb(null , JSON.parse(body));
+					if(err) sendCompletedcb({ error : err , response : response } , null );
+					else sendCompletedcb(null , JSON.parse(body));
 				});
 			}else{
-				return cb({ error : err , response : response } , null )
+			  sendConfigCb({ error : err , response : response } , null );
 			}
 		});
 	});
